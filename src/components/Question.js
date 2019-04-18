6dda7878ce8c8099a3b603b37b0d59dc247965f4
sbeltran10 <s.beltran10@uniandes.edu.co>
2019-04-18 13:45:39 -0700

    question component initial structure and elements

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { Button, ListItem } from 'react-native-elements';
-import styles from '../styles/main';
+import { View } from 'react-native';
+import { Text, Button, ListItem } from 'react-native-elements';
+import styles from '../styles/Question';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
-export default (props) => (
-  <View style={styles.style1}>
-    <Text>Question Screen</Text>
-    <Button title="Next"/>
+export default ({ question, answers, selectAnswer, submitAnswer, modalVisible, selectedAnswerId }) => (
+  <View style={styles.mainView}>
+    <View>
+      <Text h4>{question.content}</Text>
+    </View>
+    <View>
+      <ScrollView>
+        {
+          answers.map((answer, i) => (
+            <ListItem
+              key={i}
+              leftIcon={selectedAnswerId === answer.id ?
+                { name: 'radio_button_checked' }
+                :
+                { name: 'radio_button_unchecked' }}
+              title={item.title}
+              onPress={() => selectAnswer(i)}
+              containerStyle={selectedAnswerId === answer.id ?
+                styles.answerContainerSelected
+                :
+                styles.answerContainer
+              }
+            />
+          ))
+        }
+      </ScrollView>
+      <Button
+        onPress={submitAnswer} >
+        Submit answer
+      </Button>
+    </View>
+    <Overlay
+      isVisible={modalVisible}
+      windowBackgroundColor="rgba(255, 255, 255, .5)"
+      width="auto"
+      height="auto"
+    ></Overlay>
   </View>
 )
 
---
+++ src/styles/Question.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  answerContainer: {
+    backgroundColor: 'white'
+  },
+  answerContainerSelected: {
+    backgroundColor: '#d8d8ff'
+  }
+})
