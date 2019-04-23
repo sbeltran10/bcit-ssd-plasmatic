8bccf35cda1f8e4db17d9eeab8fa9c960f8ffaff
Carson Yan <cybc00@hotmail.com>
2019-04-23 13:04:21 -0700

    working tree cleaning

--- src/components/QuestionnaireList.js
+++ src/components/QuestionnaireList.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
-// import styles from '../styles/QuestionnaireList';
+import styles from '../styles/QuestionnaireList';
 
 export default (props) => (
     <View>
         {props.questionnaires.map((item, key) => (
             <Text
+                // style={styles.listContainer}
                 key={key} 
                 onPress={()=>(props.onSelect(item.id))}>{item.title}
             </Text>
--- src/styles/QuestionnaireList.js
+++ src/styles/QuestionnaireList.js
@@ -1,14 +1,18 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
+    listContainer: {
         flex: 1,
+        justifyContent: 'center',
         backgroundColor: '#F5FCFF',
+        borderWidth: 1,
+        borderColor: 'black',
+        borderStyle: 'dashed'
       },
       items: {
-        fontSize: 15,
-        textAlign: 'center',
-        margin: 10,
+        // fontSize: 15,
+        // textAlign: 'center',
+        // margin: 10,
       },
       button: {
         width: 200
