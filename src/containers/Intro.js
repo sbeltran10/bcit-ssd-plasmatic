ad99e348d2583f062a2e5de90625ff6547d93b22
Carson Yan <cybc00@hotmail.com>
2019-04-24 13:51:49 -0700

    adds react-native-element card component in Intro component

--- src/containers/Intro.js
+++ src/containers/Intro.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Button, ListItem } from 'react-native-elements';
+import { Card, ListItem, Button, Icon } from 'react-native-elements';
 // import styles from '../styles/main';
 
 class Intro extends Component {
@@ -8,13 +8,30 @@ class Intro extends Component {
   render() {
     return (
       <View style={{backgroundColor: 'green'}}>
-          <Text>{this.props.questionnaire[0].title}</Text>
+          {/* <Text>{this.props.questionnaire[0].title}</Text>
           <Text>{this.props.questionnaire[0].description}</Text>
           <Button title="Start" onPress={()=> {
             let step = 'question'
             let fetchFirstQuestion = this.props.fetchFirstQuestion;
             fetchFirstQuestion(step);
-          }} />
+          }} /> */}
+
+      <Card title={this.props.questionnaire[0].title}>
+        <Text style={{marginBottom: 10}}>
+          {this.props.questionnaire[0].description}
+        </Text>
+        <Button
+          icon={<Icon name='code' color='#ffffff' />}
+          backgroundColor='#03A9F4'
+          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+          title="Start" onPress={()=> {
+            let step = 'question'
+            let fetchFirstQuestion = this.props.fetchFirstQuestion;
+            fetchFirstQuestion(step);
+            }
+          }
+        />
+      </Card>
       </View>
     )
   }
