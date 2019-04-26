94fbc2add2c5605cafe7e887676a9377d5c8560d
sbeltran10 <s.beltran10@uniandes.edu.co>
2019-04-26 09:58:45 -0700

    api request function structure

--- src/api/QuestionAPI.js
+++ src/api/QuestionAPI.js
@@ -1,7 +1,23 @@
-const QuestionsAPI = {};
+let QuestionsAPI = {};
 
-QuestionsAPI.read = () => {
+QuestionsAPI.readById = (id) => {
   // Do fetch call
+  const baseUrl = 'https://rtwn92w64m.execute-api.us-east-2.amazonaws.com/prod'
+  fetch(`${baseUrl}?id=${id}`)
+    .then((response) => {
+      if (response.status !== 200) {
+        // Handle error response
+        return;
+      }
+
+      response.json().then((data) => {
+        // Return this data
+        console.log(data);
+      });
+    })
+    .catch((err) => {
+      // Handle this error
+    });
 }
 
 export default QuestionsAPI;
