e067cfcb52c595f018356010bc8c830a8af995fc
AJ Adversalo <ajadvers@gmail.com>
2019-05-01 11:21:17 -0700

    add documentation to index

--- src/containers/Index.js
+++ src/containers/Index.js
@@ -4,13 +4,14 @@ import { Button } from 'react-native-elements';
 import styles from '../styles/Index';
 import QuestionnaireList from '../components/QuestionnaireList';
 import QuestionnairePicker from '../components/QuestionnaireType';
+import PropTypes from 'prop-types';
 
 /**
  * This component is the initial page containing the sub components
- * which select the questionnaire category and questionnaires associated to it
+ * which allows selection of the questionnaire category and questionnaires associated to it
  */
-
-let Index = ({questionnaires, selectedQuestionnaireId, type, title, onPickerValueChange, updateSelectedQuestionnaireId, fetchQuestionnaire}) => (
+ 
+let Index = ({questionnaires = [], selectedQuestionnaireId, type, onPickerValueChange, updateSelectedQuestionnaireId, fetchQuestionnaire}) => (
     <View >
         <View style={styles.titleContainer}>
             <Text style={styles.title}>Questionnaire Selection</Text>
@@ -43,6 +44,21 @@ let Index = ({questionnaires, selectedQuestionnaireId, type, title, onPickerValu
             />  
         </View>
     </View>
-)       
-       
+)
+
+Index.propTypes = {
+    /** Array containing the queried questionnaires. */
+    questionnaires: PropTypes.array,
+    /** Primary key of the selected questionnaire. */ 
+    selectedQuestionnaireId: PropTypes.number,
+    /** String containing the questionnaire category. */ 
+    type: PropTypes.string,
+    /** callback to update the category state. */ 
+    onPickerValueChange: PropTypes.func,
+    /** callback to update the chosen questionnaire id. */ 
+    updateSelectedQuestionnaireId: PropTypes.func,
+    /** callback to retrieve the filtered questionnaires. */ 
+    fetchQuestionnaire: PropTypes.func
+};
+
 export default Index;
